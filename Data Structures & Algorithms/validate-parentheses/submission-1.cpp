de5b09bbbc9a592e436efda6ddc6ed8eae003bbd
Shaurya Thakur <thakurshaurya2007@gmail.com>
2026-07-01 14:48:06 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s[0]==']' || s[0]==')' || s[0]=='}'){
+            return false;
+        }
+        stack<char> st;
+        for(char c:s){
+            if(c == '[' || c == '{' || c == '('){
+                st.push(c);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if(c == ']' && st.top() == '['){
+                    st.pop();
+                }
+                else if(c == '}' && st.top() == '{'){
+                    st.pop();
+                }
+                else if(c == ')' && st.top() == '('){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(!st.empty()){
+            return false;
+        }
+        return true;
+    }
+};
