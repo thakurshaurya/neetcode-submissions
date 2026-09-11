51c1e809eea1bd94dac7d0d99ba33b82868d3508
Shaurya Thakur <thakurshaurya2007@gmail.com>
2026-09-11 17:10:52 +0530

    Add: length-of-last-word - submission-3

---
+++ Data Structures & Algorithms/length-of-last-word/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int maxLength = 0;
+        int counter = 0;
+        for(int i = s.size()-1; i >= 0; i--){
+            if(s[i] != ' '){
+                counter++;
+            }
+            else{
+                if(counter>0){
+                    maxLength = max(maxLength,counter);
+                    return maxLength;
+                }
+            }
+        }
+        return counter;
+    }
+};
